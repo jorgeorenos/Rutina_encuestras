76a29c06853db0314c49f77e4f1351ceeca2a3db
peloslargos302 <114953886+peloslargos302@users.noreply.github.com>
2022-12-08 09:23:27 -0600

    Proyecto comentado

--- DE-ENTREVISTAS.R
+++ DE-ENTREVISTAS.R
@@ -1,6 +1,8 @@
 ######DIRECCIONAMIENTO ESTRATÉGICO######
+
+#Cargamos la librería a utilizar.
 library(syuzhet)
-# Aquí añadimos un cambio
+
 
 #Aquí podemos introducir las respuestas de la entrevista realizada.
 respuesta <- ("Ahora ahora tal vez voy bueno bueno bueno bueno bueno a regresar un poco a lo que proponía de conocimientos básicos en Administración Pública ¿Por qué? Porque la reestructura actual eh busca como tal, tener una reestructura que responda al nuevo modelo de atención. Nosotros como el órgano técnico de la Administración pública tendríamos, o se busca ser un referente para toda la institucionalidad pública. Siendo un instituto tendríamos que dar un ordenamiento, tendríamos que ser el referente para ministerios para secretarías y demás instituciones. Sin embargo, creo muy sinceramente que estamos con un déficit de de de la capacidad de respuesta de las personas que actualmente están, hay personas que tal vez cumplen un nivel académico pero... no sé, les falta ese ese, les falta, no sé si experiencia en el ámbito público, o esa capacidad de respuesta porque, ahora bien, derivado lo que mencionas como era como como referentes tendríamos que tener unas 
@@ -9,28 +11,45 @@ delegaciones nítidas y personas con capacidad de de administrar recursos, perso
 
 servidor público, muchas personas a veces vienen desde afuera que no conocen bueno bueno bueno la administración pública como tal. Se si hacen por bueno bueno bueno bueno ejemplo si hablamos del área financiera el ámbito financiero que eso es mi competencia, es muy distinta a la contabilidad que se llevaba en empresas privadas a la contabilidad gubernamental. El ámbito presupuestario, el ciclo presupuestario que nosotros llevamos como en como estado es bueno bueno bueno bueno muy distinto a un ciclo de pago en empresas privadas. Por ejemplo la facturación, la documentación de respaldo y esa es información que obviamente no lo pueden tener a cabalidad en una certificación, pero sin embargo ya van teniendo nociones. Por ejemplo, aquí en en el estado también es un poco burócrata ese esos procedimientos pero personas que vienen de afuera ocuparon algunos cargos muchas veces de directivos piensan que que lo pueden hacer así, como que traen experiencias del ámbito privado del sector privado y no es así, tenemos que llevar un debido proceso entonces a veces es si una persona que viene como directivo no conoce eso, quiere como pasarse por alto esa esos procedimientos que muchas veces pueden resultar en hallazgos para los técnicos, para bueno bueno bueno bueno bueno bueno bueno los operativos.")
 
+
+
+#Se extraen las palabras de la respuesta cargada.
 texto_palabras <- get_tokens(respuesta)
+#Nos muestra las primeras palabras de la respuesta cargada.
 head(texto_palabras)
+#Nos devuelve el número de palabras en la respuesta cargada.
 length(texto_palabras)
 
+
+#Se extraen las oraciones de la respuesta cargada, 
 oraciones_vector <- get_sentences(respuesta)
+#Nos muestra el número de oraciones en la respuesta cargada.
 length(oraciones_vector)
+
+#Mostrar las palabras de la respuesta gargada.
 texto_palabras
+#Mostrar las oraciones de la respuesta cargada.
 oraciones_vector
 
 
+#Se genera el diccionario, éste consta de dos columnas; una para colocar la palabra, y otra para colocar el valor que se le asigna a la palabra en cuestión.
 DICCIONARIO <- data.frame(
   "word" = c("prioridad","bueno","malo", "mala gestión", "poco", "engorroso", "cumplimiento", "no", "sí", "cumple", "ayudar", "ayudamos", "juntos"),
   "value" = c(1, 3,-2,-5,-1,-2,1,-1,1,1,4,4,3))
 
 
+#Con esta instrucción se convierte el diccionario en un archivo .csv
 write.csv(x=DICCIONARIO, file = "DICCIONARIO.csv", row.names = FALSE)
 
-DICCIONARIO_DE <- read.csv("DICCIONARIO.csv")
-method <- "custom"
 
+#Se le asigna la variable DICCIONARIO_DE al archivo .csv
+DICCIONARIO_DE <- read.csv("DICCIONARIO.csv")
+#Se le asigna a la variable method, el método customizado. Esto, para poder utilizar el diccionario personalizado.
+method <- "custom"
+#Se le asigna a la variable DE_oraciones, la función get_sentiment. Aquí es donde se pondera la oración, según el diccionario personalizado.
 DE_oraciones <- get_sentiment(oraciones_vector, method = method, lexicon = DICCIONARIO_DE)
 
+#Muestra una gráfica de las ponderaciones de la respuesta cargada.
 Dplot(DE_oraciones,
      type="l",
      main = "Estrategia de Direccionamiento",
@@ -39,8 +58,8 @@ Dplot(DE_oraciones,
      )
 
 
-# Prueba
-
+####Prueba####
+#En esta prueba, analizamos palabra por palabra de la respuesta cargada, y se ponderan según el diccionario personalizado.
 oracion <- oraciones_vector[1]
 
 frases_oracion <- get_tokens(oracion)
