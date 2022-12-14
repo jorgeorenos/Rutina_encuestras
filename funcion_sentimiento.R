bd1811239adf837d420b4b4c45b2fe123be352a1
jorgeorenos <orenosjorge@gmail.com>
2022-12-13 21:52:32 -0600

    Función sentimientos, versión 2

--- funcion_sentimiento.R
+++ funcion_sentimiento.R
@@ -1,19 +1,16 @@
 # Construcción de la función para el análisis de sentimientos
-# Cargamos la libreria stringr para manipular texto
-library(stringr)
 
 sentimientos <- function(texto){
   
   texto <- tolower(texto)
-  palabras <- str_split(texto, " ")
+  palabras <- unlist(strsplit(texto, "\\W"))
+  palabras <- palabras[which(palabras != "")]
   
   diccionario <- read.csv("https://raw.githubusercontent.com/jorgeorenos/Rutina_encuestras/modificaciones/Diccionarios/diccionario_afinn_modificado.csv",
                           fileEncoding = "latin1")
   
-  Ponderacion <- diccionario[which(diccionario$Palabra %in% palabras), "Palabra", "Puntuacion", "positivas", "negativas"]
+  Ponderacion <- diccionario[which(diccionario$Palabra %in% palabras), c("Palabra","Puntuacion", "positivas", "negativas")]
   valor <- sum(Ponderacion$Puntuacion)
   
   return(list(Ponderacion, valor))
-  print("La valoración del sentimiento es")
-  print(valor)
 }
