45e93602c16cfe8cc8aac9e6aa16be4eab1bc06a
Jorge Orenos <orenosjorge@gmail.com>
2022-12-14 09:05:47 -0600

    creación diccionario nrc

--- creacion_diccionario.R
+++ creacion_diccionario.R
@@ -1,27 +1,36 @@
 # Descargamos la libreria tidyverse para trabajar los datos
 library(tidyverse)
-library(fastmatch)
-# Descargando el diccionario "afin"
+
+# Descargando el diccionario "afinn"
 ## Diccionario traducido del repositorio https://github.com/jboscomendoza/rpubs.git
 
 download.file("https://raw.githubusercontent.com/jboscomendoza/rpubs/master/sentimientos_afinn/lexico_afinn.en.es.csv",
               destfile = "Diccionarios/diccionario_afinn.csv")
 
 # Cargando el diccionario para extrer positivas y negativas
-diccionario <- read.csv("Diccionarios/diccionario_afinn.csv", fileEncoding = "latin1")
+afinn <- read.csv("Diccionarios/diccionario_afinn.csv", fileEncoding = "latin1")
 
 # Mutate para generar nuevas columas
 ## Columnas con el valor absoluto de cada palabra.
-diccionario <- diccionario %>% mutate("positivas" = ifelse(Puntuacion >= 0, Puntuacion, 0),
+afinn <- afinn %>% mutate("positivas" = ifelse(Puntuacion >= 0, Puntuacion, 0),
                                       "negativas" = ifelse(Puntuacion < 0, abs(Puntuacion), 0))
 
 # Identificación de palabras positivas y negativas
-positivas <- diccionario %>% filter(Puntuacion >= 0) %>% select(Palabra, Puntuacion)
-negativas <- diccionario %>% filter(Puntuacion < 0) %>% select(Palabra, Puntuacion)
+positivas <- afinn %>% filter(Puntuacion >= 0) %>% select(Palabra, Puntuacion)
+negativas <- afinn %>% filter(Puntuacion < 0) %>% select(Palabra, Puntuacion)
 
 # comprobamos que la imputación fue correcta
-sum(diccionario$negativas) + sum(negativas$Puntuacion)
-sum(diccionario$positivas) - sum(positivas$Puntuacion)
+sum(afinn$negativas) + sum(negativas$Puntuacion)
+sum(afinn$positivas) - sum(positivas$Puntuacion)
 
-write.csv(diccionario, file = "Diccionarios/diccionario_afinn_modificado.csv",
+write.csv(afinn, file = "Diccionarios/diccionario_afinn_modificado.csv",
+          fileEncoding = "latin1")
+
+
+# CREACION DICCIONARIO "NRC"
+
+# Cargando el diccionario
+nrc <- read.table("Diccionarios/Spanish-NRC-Emolex.txt", header = TRUE, sep = "\t")
+
+write.csv(nrc, file = "Diccionarios/diccionario_nrc.csv",
           fileEncoding = "latin1")
--- funcion_sentimiento.R
+++ funcion_sentimiento.R
@@ -1,15 +1,18 @@
 # Construcción de la función para el análisis de sentimientos
 
-sentimientos <- function(texto){
+
+
+sentimientos <- function(texto, diccionario = "nrc"){
   
   texto <- tolower(texto)
   palabras <- unlist(strsplit(texto, "\\W"))
   palabras <- palabras[which(palabras != "")]
   
-  diccionario <- read.csv("https://raw.githubusercontent.com/jorgeorenos/Rutina_encuestras/modificaciones/Diccionarios/diccionario_afinn_modificado.csv",
+  
+  afinn <- read.csv("https://raw.githubusercontent.com/jorgeorenos/Rutina_encuestras/modificaciones/Diccionarios/diccionario_afinn_modificado.csv",
                           fileEncoding = "latin1")
   
-  Ponderacion <- diccionario[which(diccionario$Palabra %in% palabras), c("Palabra","Puntuacion", "positivas", "negativas")]
+  Ponderacion <- as.data.frame(diccionario[which(diccionario$Palabra %in% palabras), c("Palabra","Puntuacion", "positivas", "negativas")])
   valor <- sum(Ponderacion$Puntuacion)
   
   return(list(Ponderacion, valor))
